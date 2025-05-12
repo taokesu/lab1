1011ed93ece540c8b3ef1ad78a4ad297236e4bf9
Denis <denis@gmail.com>
2025-05-12 21:53:13 +0300

    All other math operations

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 def calculate(a, b, op):
     if op == '+': return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    elif op == '/':
+        if b == 0:
+            raise ValueError("Деление на ноль невозможно")
+    return a / b
+
+    else:
+        raise ValueError("Неверная операция")
